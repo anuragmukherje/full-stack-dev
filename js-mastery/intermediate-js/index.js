3d798900776323917cef8186c4402ad19a44d635
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-05 19:48:01 +0530

    Restructuring of the file structure

---
+++ js-mastery/intermediate-js/index.js
@@ -0,0 +1,15 @@
+// Arrays -> 0-based index
+
+// Number array
+// const myList = [2,3,4,5,6];
+// console.log(myList);
+
+// String array
+// const stringArr = ["eat", "code", "sleep", "repeat"];
+// console.log(stringArr);
+
+// // Accessing our items from the array
+// console.log(stringArr[0]);
+// console.log(stringArr[1]);
+// console.log(stringArr[2]);
+// console.log(stringArr[3]);
