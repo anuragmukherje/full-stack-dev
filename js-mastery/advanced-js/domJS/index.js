600fd04e29986fba3ffc88b9de95a337b0cd3adc
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-18 11:17:45 +0530

    Adding new module codes

---
+++ js-mastery/advanced-js/domJS/index.js
@@ -0,0 +1,15 @@
+// When a web page is loaded, the browser creates a Document Object Model of the page.
+// In the browser, the document object is a fundamental part of the Document Object Model (DOM). 
+// It represents the web page currently loaded in the browser and provides a way to 
+// interact with and manipulate its content.
+
+// The DOM abstracts the structure of the document into a tree of objects, allowing scripts to 
+// manipulate the content and structure dynamically. 
+// This abstraction enables more complex interactions and functionalities beyond just static HTML.
+
+
+console.log(document);
+console.log(document.head);
+console.log(`title is : ${document.title}`);
+
+console.log(document.body);
