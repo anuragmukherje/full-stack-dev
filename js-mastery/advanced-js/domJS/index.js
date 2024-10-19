ffd93c21682bfda636b946eb372a7774d735c56f
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-19 20:00:18 +0530

    New module code pushed

--- js-mastery/advanced-js/domJS/index.js
+++ js-mastery/advanced-js/domJS/index.js
@@ -8,8 +8,41 @@
 // This abstraction enables more complex interactions and functionalities beyond just static HTML.
 
 
-console.log(document);
-console.log(document.head);
-console.log(`title is : ${document.title}`);
+// console.log(document);
+// console.log(document.head);
+// console.log(`title is : ${document.title}`);
 
-console.log(document.body);
+// console.log(document.body);
+// console.log(document.documentURI);
+
+
+
+
+// ACCESSING THE DOM
+//****** DOM SELECTORS *******
+// // 1. getElementsByTagName
+// console.log(document.getElementsByTagName("h1"));
+
+// // 2. getElementById
+// console.log(document.getElementById("main"));
+
+// // 3. getElementsByClassName
+// console.log(document.getElementsByClassName("cls"));
+// console.log(document.getElementsByClassName("cls-1"));
+
+// // 4. querySelector
+// console.log(document.querySelector("h1"));         // For TagName
+
+// console.log(document.querySelector(".cls"));       // For ClassName
+// console.log(document.querySelector(".cls-1"));     // For className
+
+// console.log(document.querySelector("#main"));      // For Id
+// console.log(document.querySelector("#id-1"));      // For Id
+
+// // 5. querySelectorAll
+
+// console.log(document.querySelector("h1"));
+// console.log(document.querySelectorAll("h1"));
+
+// console.log(document.querySelectorAll("li"));
+
