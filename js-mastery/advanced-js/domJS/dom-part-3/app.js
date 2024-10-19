88a03bdf1d40b1729ad108292fc9ea2bf339e68f
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-19 21:17:59 +0530

    Added DOM part-3 modules

---
+++ js-mastery/advanced-js/domJS/dom-part-3/app.js
@@ -0,0 +1,15 @@
+// const paraSelect = document.querySelector("p");
+// // => gives pure tect from the html document
+// console.log(paraSelect.innerText);
+
+// // => gives the pure content from the html document
+// console.log(paraSelect.textContent); 
+
+// // => gives everything from the html document
+// console.log(paraSelect.innerHTML);
+
+
+// const hTagEl = document.querySelector("h1");
+// hTagEl.innerText = "Hi there Welcome";
+
+// hTagEl.innerHTML = "<em> Hello There!! </em>" ;
---
+++ js-mastery/advanced-js/domJS/dom-part-3/exercise/app.js
@@ -0,0 +1,12 @@
+
+const firstEl = document.querySelector(".first");
+// Gives the innerText of the element
+console.log(firstEl.innerText);
+
+const secondEl = document.querySelector(".second");
+// Gives the textContent of the element
+console.log(secondEl.textContent);
+
+const thirdEl = document.querySelector(".third");
+// Gives the innerHTML of the element
+console.log(thirdEl.innerHTML);
