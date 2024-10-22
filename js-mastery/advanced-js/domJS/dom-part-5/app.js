46fa442af77344ca84d30171f8dd8df2f2137159
Anurag Mukherjee <amdevremote@gmail.com>
2024-10-22 23:33:37 +0530

    Adding code to DOM modules

---
+++ js-mastery/advanced-js/domJS/dom-part-5/app.js
@@ -0,0 +1,20 @@
+// // get the href 
+// let aTagEl = document.querySelector("a");
+// console.log(aTagEl.href);
+
+const inpEl = document.querySelector("input");
+// console.log(inpEl.value); // gives nothing as it has no value
+// console.log(inpEl.type);
+
+
+// aTagEl.href = "https://www.youtube.com/@huxnwebdev";
+// aTagEl.innerHTML = "HuXn DevPro";
+
+// inpEl.type = "password";
+// console.log(inpEl.value);
+
+// console.log(aTagEl.href);
+
+
+// getAttribute(attrName)
+console.log(inpEl.getAttribute("type"));
